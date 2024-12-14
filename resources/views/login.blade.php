66348da284b3f28856ffa3fb6311cdcb9f805e00
Shivam_Singh <shivamsingh6490@gmail.com>
2024-12-14 20:08:47 +0530

    login merge conflict solved

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,12 +1,6 @@
 @includeIf("pages.header")
 <div class="login">
-<<<<<<< HEAD
-=======
-    {{-- @if ($success)
-        {{'your account has been created'}}
-    @endif --}}
-    {{}}
->>>>>>> 188252b71dc6903aedd5d7c071aaff32081b31d7
+
     <div class="col-4 bg-white border rounded p-4 shadow-sm">
         <form method="POST" action="">
             @csrf
@@ -15,7 +9,7 @@
                 <img class="mb-4" src="img/pictogram.png" alt="" height="45">
             </div>
             <h1 class="h5 mb-3 fw-normal">Please sign in</h1>
-<<<<<<< HEAD
+
             @if(session()->has('error'))
             <div class="alert alert-danger">{{session()->get('error')}}</div>
             @endif
@@ -27,19 +21,12 @@
                 <label for="floatingInput">username/email</label>
                 <span class="text-danger"> <small>@error('email')
                     {{$message}}
-                @enderror </small></span>
-=======
-
-            <div class="form-floating">
-                <input type="email" class="form-control rounded-0" name="username_email" placeholder="username/email">
-                <label for="floatingInput">username/email</label>
->>>>>>> 188252b71dc6903aedd5d7c071aaff32081b31d7
-            </div>
+                    @enderror </small></span>
+                </div>
 
             <div class="form-floating mt-1">
                 <input type="password" class="form-control rounded-0" name="password" id="floatingPassword" placeholder="Password">
                 <label for="floatingPassword">password</label>
-<<<<<<< HEAD
                 <span class="text-danger"> <small>@error('password')
                     {{$message}}
                 @enderror </small></span>
@@ -51,16 +38,6 @@
 
 
             </div>  
-=======
-            </div>
-
-            <div class="mt-3 d-flex justify-content-between align-items-center">
-                <button class="btn btn-primary" type="submit">Log in</button>
-                <a href="{{ url('/signup')}}" class="text-decoration-none">Create New Account</a>
-
-
-            </div>
->>>>>>> 188252b71dc6903aedd5d7c071aaff32081b31d7
             <a href="#" class="text-decoration-none">Forgot password ?</a>
         </form>
     </div>
